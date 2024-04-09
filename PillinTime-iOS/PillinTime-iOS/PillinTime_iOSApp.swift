038c30311621d5481820538e82990efad9e74a39
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-04-09 15:39:06 +0900

    [Setting] #1 - Xcode 프로젝트 생성

---
+++ PillinTime-iOS/PillinTime-iOS/PillinTime_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillinTime_iOSApp.swift
+//  PillinTime-iOS
+//
+//  Created by Jae Hyun Lee on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PillinTime_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
